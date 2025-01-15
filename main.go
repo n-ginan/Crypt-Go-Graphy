92ede19225894834d698d47645fbdc9c5497b66f
papsykle <toramgarcia@gmail.com>
2025-01-15 20:08:47 +0800

    feat(vigenere): Vigenere Encryption has been implemented--but, not yet fully tested if its right

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"os"
 )
 
+/*
+	Things to remember or go back to:
+	- Go back to affine function ciphers to learn more about the gcd(x, y) != 1
+	- Prove the vigenere encryption
+*/
+
 func ShiftEncryption(text string, shift rune) string {
 	var cipherText string
 	for _, char := range text {
@@ -48,6 +54,7 @@ func AffineEncryption(text string, a rune, b rune) string {
 	// The formula for this is just an affine function (ax + b)
 	// I should go back into this because there are still a lot to understand
 	// such as the euclidian algorithm to find the gcd
+	// This encryption must have a one-to-one cardinality, use input and alter as the word (they have the same encryption)
 	var cipherText string
 	for _, char := range text {
 		cipherText += string(((a * (char - 97) + b) % 26) + 97) // Continue Latur The Quick Maffs
@@ -60,6 +67,18 @@ func AffineDecryption(text string) string {
 	var plainText string
 	for _, char := range text {
 		plainText += string(((3 * (char - 97) + 20) % 26) + 97)
+	}
+	return plainText
+}
+
+func VigenereEncryption(text string, length byte, vectors ...byte) string {
+	// Still unsure if this is right, I havent done the math implementation yet
+	if length <= 0 && length >= 26 {
+		return "Invalid length number"
+	}
+	var plainText string
+	for position, char := range text {
+		plainText += string(((char - 97+ rune(vectors[position % len(vectors)])) % 26) + 97)
 	}
 	return plainText
 }
@@ -85,7 +104,7 @@ func Main() {
 		}
 	}()
 	buf := make([]byte, 1 << 10)
-	var output string
+	var output string	
 	for {
 		n, err := fi.Read(buf)
 		if err == io.EOF {
@@ -99,7 +118,7 @@ func Main() {
 			output += string(buf[:n])
 		}
 	}
-	testString := AffineEncryption("itonlyworkswithsmallletters", 9, 2)
+	testString := VigenereEncryption("hotdoggers", 5, 12, 15, 17, 2, 16, 22, 24)
 	fmt.Print(testString)
 	/*for position, _ := range testString { a = 9, b = 2
 		fmt.Println(testString[position:position+12])
